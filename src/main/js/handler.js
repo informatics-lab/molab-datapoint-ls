9f19b92c18695ea70f33ff58e3629a5affb39253
Thomas Powell <t.powell.meto@gmail.com>
2017-01-11 13:25:58 +0000

    geocodes location

--- src/main/js/handler.js
+++ src/main/js/handler.js
@@ -2,29 +2,36 @@
 
 import Validator from "./validators/Validator";
 import * as schema from "./schemas/request.json";
+
 import JsonResponse from "./responses/JsonResponse";
 import TextResponse from "./responses/TextResponse";
-// import Geocoder from "./geo/Geocoder";
+
+import Geocoder from "./geo/Geocoder";
+
 
 const validator = new Validator(schema);
-// const geocoder = new Geocoder();
+const geocoder = new Geocoder();
+
 
 module.exports.datapoint = (event, context, callback) => {
-
-    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
-    console.log(context);
-    console.log(callback);
 
     if (event.queryStringParameters) {
         validator.validate(event.queryStringParameters)
             .then(() => {
-                callback(null, new JsonResponse({valid:"ok"}));
+                return geocoder.getLatLng(event.queryStringParameters.location)
+            })
+            .then((latlng) => {
+                const response = {
+                    location: event.queryStringParameters.location,
+                    latlng: latlng
+                };
+                callback(null, new JsonResponse(response))
             })
             .catch((err) => {
                 callback(null, new TextResponse(err.toString()));
             });
     } else {
-        callback(null, new JsonResponse({message: "no query string found!"}));
+        callback(null, new JsonResponse({message: "missing location query param"}));
     }
 
 };
